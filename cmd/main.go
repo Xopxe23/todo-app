dc2695bc9decc56d61085b4f2ec12a41e74e0422
George Dudaev <georgeduday@gmail.com>
2023-08-04 12:32:12 +0300

    create handler and init routes

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/Xopxe23/todo-app"
+	"github.com/Xopxe23/todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ocured while running http server: %s", err.Error())
 	}
 }
