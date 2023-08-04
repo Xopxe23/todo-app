298d5c19df072d6fec2b3781092489b6658d5130
George Dudaev <georgeduday@gmail.com>
2023-08-04 19:14:24 +0300

    create db connection, env and yml files

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 
 	"github.com/Xopxe23/todo-app"
 	"github.com/Xopxe23/todo-app/pkg/handler"
@@ -14,7 +18,22 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initialization configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err!= nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
