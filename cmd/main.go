d997ef99ffc8183232b2f8b1da177cb96a9e5fee
George Dudaev <georgeduday@gmail.com>
2023-08-04 16:07:02 +0300

    create repos and service structs

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,27 @@ import (
 
 	"github.com/Xopxe23/todo-app"
 	"github.com/Xopxe23/todo-app/pkg/handler"
+	"github.com/Xopxe23/todo-app/pkg/repository"
+	"github.com/Xopxe23/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initialization configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ocured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
