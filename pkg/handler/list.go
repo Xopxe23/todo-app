aeee612c3f1d52a39aa193a3dfe3b0cb91ae6daf
George Dudaev <georgeduday@gmail.com>
2023-08-07 11:13:49 +0300

    make createList funcs for Service and Repos

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,28 +3,45 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Xopxe23/todo-app"
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Handler) createList(c *gin.Context){
-	id, _ := c.Get(userCtx)
+func (h *Handler) createList(c *gin.Context) {
+	id, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err = h.services.TodoList.Create(id, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
 }
 
-func (h *Handler) getAllLists(c *gin.Context){
+func (h *Handler) getAllLists(c *gin.Context) {
 
 }
 
-func (h *Handler) getListById(c *gin.Context){
+func (h *Handler) getListById(c *gin.Context) {
 
 }
 
-func (h *Handler) updateList(c *gin.Context){
+func (h *Handler) updateList(c *gin.Context) {
 
 }
 
-func (h *Handler) deleteList(c *gin.Context){
+func (h *Handler) deleteList(c *gin.Context) {
 
 }
